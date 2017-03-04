1a9a7836d2bfe819b324f49e180643021cfc0a03
jianglei01 <18810514212@163.com>
2017-03-04 10:27:56 +0800

    测试

--- SpringBoot/src/main/java/main/Application.java
+++ SpringBoot/src/main/java/main/Application.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
